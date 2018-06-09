de06b07dc2f528aba74458049b055088f4da559d
jarretbryan <jarret.bryan@gmail.com>
2018-06-09 17:30:44 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
   #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attr_hash)
+    attr_hash.each do |stat, stat_value|
+      self.send("#{stat}=", stat_value) 
+    end
+  end
+
 end
